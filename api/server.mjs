62f85b6071a8efc117234859f7291fffee49b43c
Falling <135897083+F-alling@users.noreply.github.com>
2025-01-17 20:40:15 -0600

    Update server.mjs

--- api/server.mjs
+++ api/server.mjs
@@ -1,4 +1,4 @@
-import Redis from 'ioredis'
+
 import config from '../base/config.mjs'
 import Parent from '../base/server.mjs'
 import StaticRoutes from '../base/static_routes.mjs'
@@ -12,28 +12,5 @@ export default class Server extends Parent {
 
     this.flaskaOptions.appendHeaders['Content-Security-Policy'] = `default-src 'self'; script-src 'self' 'nonce-0d1valZOnjp8ZpR6vBd4dg=='; style-src 'self' 'unsafe-inline'; img-src * data: blob:; media-src *; font-src 'self' data:; object-src 'none'; frame-ancestors 'none'`
     this.flaskaOptions.appendHeaders['Cross-Origin-Embedder-Policy'] = 'unsafe-none'
-    this.redis = new Redis(config.get('redis'))
-    this.redis.on('error', (err) => {
-      this.core.log.error(err)
-    })
-    this.routes = {
-      static: new StaticRoutes(),
-      post: new IndexPost({
-        frontend: config.get('frontend:url'),
-      }),
-    }
-    this.routes.serve = new ServeHandler({
-      root: localUtil.getPathFromRoot('../public'),
-      version: this.core.app.running,
-      frontend: config.get('frontend:url'),
-    })
-  }
-
-  runCreateServer() {
-    super.runCreateServer()
-    
-    this.flaska.before((ctx) => {
-      ctx.redis = this.redis
-    })
   }
 }
